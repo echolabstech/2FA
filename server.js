ce7b996fd35d7fd25ef658d39661ab248d37302f
major <echolabstech@gmail.com>
2020-07-07 18:33:36 +0800

    naive implementation - server side totp validation

--- server.js
+++ server.js
@@ -2,6 +2,31 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const otplib = require('otplib');
+const secret = 'KVKFKRCPNZQUYMLXOVYDSQKJKZDTSRLD';
+// Alternative:
+// const secret = otplib.authenticator.generateSecret();
+// Note: .generateSecret() is only available for authenticator and not totp/hotp
+function validate(token) {
+	let isValid = false;
+	try {
+	  isValid = otplib.authenticator.check(token, secret);
+	} catch (err) {
+	  // Possible errors
+	  // - options validation
+	  // - "Invalid input - it is not base32 encoded string" (if thiry-two is used)
+	  console.error(err);
+	}
+  return isValid;
+}
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/auth', (req, res) => {
+	const token = otplib.authenticator.generate(secret);
+	const isValid = validate(token);
+	const response = '' + token + ' ' + isValid;
+	res.send(response);
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
